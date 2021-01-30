1407bd357901dc56cb222e08b0c29dfc1506749c
crazstrom <1415441419@qq.com>
2021-01-30 19:12:09 +0800

    test1

--- main.go
+++ main.go
@@ -1,10 +1,22 @@
 package main
 
-import "net/http"
+import (
+	"io/ioutil"
+	"log"
+	"net/http"
+)
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte("I love you"))
 	})
+	http.HandleFunc("/wechat", wechat)
 	http.ListenAndServe(":8080", nil)
+}
+func wechat(w http.ResponseWriter, r *http.Request) {
+	var content, _ = ioutil.ReadAll(r.Body)
+	defer r.Body.Close()
+
+	log.Println(content)
+	w.Write([]byte("crazstom"))
 }
