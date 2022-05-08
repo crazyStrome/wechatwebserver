037af84549626319473e2026c81a59ca57f25017
haadyhan <haadyhan@tencent.com>
2022-05-08 14:46:00 +0800

    直接返回echostr

--- main.go
+++ main.go
@@ -2,12 +2,9 @@ package main
 
 import (
 	"context"
-	"crypto/sha1"
 	"io/ioutil"
 	"log"
 	"net/http"
-	"sort"
-	"strings"
 	"wechatwebserver/config"
 	"wechatwebserver/token"
 
@@ -26,20 +23,12 @@ func main() {
 	}
 	r := gin.Default()
 	r.GET("/wechat", func(c *gin.Context) {
-		sig := c.Query("signature")
-		timestamp := c.Query("timestamp")
-		nonce := c.Query("nonce")
 		echostr := c.Query("echostr")
-		token := config.GetConf().Token
-		arr := []string{token, timestamp, nonce}
-		sort.Strings(arr)
-		sha := sha1.Sum([]byte(strings.Join(arr, "")))
-		if string(sha[:]) == sig {
-			c.String(http.StatusOK, echostr)
-		}
+		c.String(http.StatusOK, echostr)
 	})
 	r.Run(config.GetConf().Addr)
 }
+
 func wechat(w http.ResponseWriter, r *http.Request) {
 	var content, _ = ioutil.ReadAll(r.Body)
 	defer r.Body.Close()
