702653b663743e364418d3438d40042be9ba1c0e
haandy <1415441419@qq.com>
2023-12-31 23:15:51 +0800

    add cost

--- client/open_ai.go
+++ client/open_ai.go
@@ -2,6 +2,7 @@ package client
 
 import (
 	"context"
+	"time"
 	"wechatwebserver/config"
 
 	openai "github.com/sashabaranov/go-openai"
@@ -12,13 +13,13 @@ var openAICli *openai.Client
 
 func Init() error {
 	cfg := openai.DefaultConfig(config.GetConf().OpenAIConfig.Token)
-    cfg.BaseURL = config.GetConf().OpenAIConfig.URL
-    openAICli = openai.NewClientWithConfig(cfg)
-    return nil
+	cfg.BaseURL = config.GetConf().OpenAIConfig.URL
+	openAICli = openai.NewClientWithConfig(cfg)
+	return nil
 }
 
 func Test(ctx context.Context) {
-    resp, err := openAICli.CreateChatCompletion(
+	resp, err := openAICli.CreateChatCompletion(
 		context.Background(),
 		openai.ChatCompletionRequest{
 			Model: openai.GPT3Dot5Turbo,
@@ -40,19 +41,30 @@ func Test(ctx context.Context) {
 }
 
 func Talk(ctx context.Context, reqMsg string) (string, error) {
-    ddl, ok := ctx.Deadline()
-    logrus.Infof("Talk ddl:%v, ok:%v", ddl, ok)
-    rsp, err := openAICli.CreateChatCompletion(ctx, openai.ChatCompletionRequest{
-        Model: openai.GPT3Dot5Turbo,
-        Messages: []openai.ChatCompletionMessage{
-            {
-                Role: openai.ChatMessageRoleUser,
-                Content: reqMsg,
-            },
-        },
-    })
-    if err != nil {
-        return "", err
-    }
-    return rsp.Choices[0].Message.Content, nil
+	ch := make(chan string)
+	now := time.Now()
+	go func() {
+		rsp, err := openAICli.CreateChatCompletion(ctx, openai.ChatCompletionRequest{
+			Model: openai.GPT3Dot5Turbo,
+			Messages: []openai.ChatCompletionMessage{
+				{
+					Role:    openai.ChatMessageRoleUser,
+					Content: reqMsg,
+				},
+			},
+		})
+		logrus.Infof("Talk:%v cost:%v, err:%v", reqMsg, time.Since(now), err)
+		if err != nil {
+			logrus.Errorf("Talk:%v err:%v", reqMsg, err)
+			return
+		}
+		ch <- rsp.Choices[0].Message.Content
+	}()
+	select {
+	case <-ctx.Done():
+		logrus.Infof("Talk:%v cost:%v, ctx.done", reqMsg, time.Since(now))
+		return "", nil
+	case data := <-ch:
+		return data, nil
+	}
 }
